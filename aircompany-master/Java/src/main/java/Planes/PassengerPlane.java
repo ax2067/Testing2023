96ddbcbc27f896ff78110d6b676c20fe57e2142e
Anna Zhuk <annazhuk2067@gmail.com>
2023-10-30 19:44:39 +0300

    Update PassengerPlane.java

--- aircompany-master/Java/src/main/java/Planes/PassengerPlane.java
+++ aircompany-master/Java/src/main/java/Planes/PassengerPlane.java
@@ -4,17 +4,14 @@ import java.util.Objects;
 
 public class PassengerPlane extends Plane{
 
-    //=================FIELDS=================
     private int passengersCapacity;
 
-    //=================CONSTRUCTORS=================
-    public PassengerPlane(String model, int maxSpeed, int maxFlightDistance, int maxLoadCapacity, int passengersCapacity) {
-        super(model, maxSpeed, maxFlightDistance, maxLoadCapacity);
+    public PassengerPlane(Plane plane) {
+        super(plane.model, plane.maxSpeed, plane.maxFlightDistance, plane.maxLoadCapacity);
         this.passengersCapacity = passengersCapacity;
     }
 
 
-    //=================METHODS=================
     public int getPassengersCapacity() {
         return passengersCapacity;
     }
@@ -26,19 +23,13 @@ public class PassengerPlane extends Plane{
                 '}');
     }
 
-//    @Override
-//    public String toString() {
-//        return super.toString().replace("}",
-//                ", passengersCapacity=" + passengersCapacity +
-//                        '}');
-//    }
 
     @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof PassengerPlane)) return false;
-        if (!super.equals(o)) return false;
-        PassengerPlane plane = (PassengerPlane) o;
+    public boolean equals(Object object) {
+        if (this == object) return true;
+        if (!(object instanceof PassengerPlane)) return false;
+        if (!super.equals(object)) return false;
+        PassengerPlane plane = (PassengerPlane) object;
         return passengersCapacity == plane.passengersCapacity;
     }
 
