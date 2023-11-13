be2b640576ceec8407727c2fb72b2a0a7658c1a4
Anna Zhuk <annazhuk2067@gmail.com>
2023-11-13 09:48:55 +0300

    added files

--- aircompany-master/Java/src/main/java/Planes/PersonalPlane.java
+++
@@ -1,8 +0,0 @@
-package Planes;
-
-public class PersonalPlane  extends Plane {
-    public PersonalPlane(Plane plane) {
-        super(plane.model, plane.maxSpeed, plane.maxFlightDistance, plane.maxLoadCapacity);
-    }
-    
-}
